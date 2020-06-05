1dc282e0f7a00a2be5a5a2ba2ad5d1047628ae07
Amir <ar.anjomshoaa@gmail.com>
2020-06-04 18:43:07 -0700

    First Push & BFS Assign

---
+++ BFS/Level_Order.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+        //Need the main ArrayList to hold the result as Expected
+        List<List<Integer>> result = new ArrayList<>();
+        
+        //If Tree empty
+        if(root==null){
+        
+            return result;
+        
+        }
+
+        Queue<TreeNode> treeTracking = new LinkedList<>();
+
+        treeTracking.add(root);
+
+        while(!treeTracking.isEmpty()){
+
+            //Array to hold each level 
+            List<Integer> eachLevel = new ArrayList<>();
+
+            
+            int sizeOfQueue = treeTracking.size();
+            
+            for(int i=0; i<sizeOfQueue; i++{
+
+                //Retreiving the head of the Queue to check for childs
+                TreeNode tempTree = treeTracking.poll();
+                eachLevel.add(tempTree.val);
+
+                if(tempTree.left != null){
+                    treeTracking.add(tempTree.left);
+                }
+                if(tempTree.right != null){
+                    treeTracking.add(tempTree.right);
+                }
+            } 
+            result.add(0,eachLevel);
+        }
+
+        return result;
+    }
+}
